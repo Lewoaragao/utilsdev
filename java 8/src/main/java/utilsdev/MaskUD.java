daa9c922481b73f03ef91f2beca573bbc04f8339
Leonardo Aragão <lewoaragao@gmail.com>
2023-10-23 13:49:00 -0300

    [feat] flow initialization with masks

---
+++ java 8/src/main/java/utilsdev/MaskUD.java
@@ -0,0 +1,29 @@
+package utilsdev;
+
+public class MaskUD {
+
+	/**
+	 * Formata um CNPJ com uma máscara específica.
+	 *
+	 * @param cnpj O CNPJ a ser formatado, representado como uma string de 14
+	 *             dígitos numéricos.
+	 * @return O CNPJ formatado com a máscara "XX.XXX.XXX/XXXX-XX".
+	 * @throws IllegalArgumentException Se o CNPJ não conter 14 dígitos numéricos.
+	 */
+	public static String formatCNPJ(String cnpj) {
+		if (cnpj == null) {
+			return "";
+		}
+
+		// Remove caracteres não numéricos
+		cnpj = cnpj.replaceAll("[^0-9]", "");
+
+		if (cnpj.length() != 14) {
+			throw new IllegalArgumentException("O CNPJ deve conter 14 dígitos numéricos.");
+		}
+
+		// Aplica a máscara de formatação
+		return cnpj.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})", "$1.$2.$3/$4-$5");
+	}
+
+}
---
+++ java 8/src/main/java/utilsdev/test/MaskUDTest.java
@@ -0,0 +1,33 @@
+package utilsdev.test;
+
+import junit.framework.TestCase;
+import utilsdev.MaskUD;
+
+public class MaskUDTest extends TestCase {
+
+	/**
+	 * Teste para verificar se o CNPJ é formatado corretamente.
+	 */
+	public void testFormatCNPJ() {
+		String cnpj = "12345678901234";
+		String formattedCNPJ = MaskUD.formatCNPJ(cnpj);
+		assertEquals("12.345.678/9012-34", formattedCNPJ);
+
+		cnpj = "1234567890";
+		try {
+			formattedCNPJ = MaskUD.formatCNPJ(cnpj);
+			fail("Deve lançar uma IllegalArgumentException para o tamanho inválido do CNPJ.");
+		} catch (IllegalArgumentException e) {
+			assertEquals("O CNPJ deve conter 14 dígitos numéricos.", e.getMessage());
+		}
+
+		cnpj = "1234567890A1234";
+		try {
+			formattedCNPJ = MaskUD.formatCNPJ(cnpj);
+			fail("Deve lançar uma IllegalArgumentException para caracteres não numéricos.");
+		} catch (IllegalArgumentException e) {
+			assertEquals("O CNPJ deve conter 14 dígitos numéricos.", e.getMessage());
+		}
+	}
+
+}
